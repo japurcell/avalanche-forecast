5d0660b4b8e325f512aac77157ba3528ff9898c0
japurcell <japurcell@users.noreply.github.com>
2021-02-14 17:11:16 -0800

    Starting on warnings

--- dist/js/components/Zones.js
+++ dist/js/components/Zones.js
@@ -1,12 +1,40 @@
-import { html, render } from 'lit-html';
+import { html, nothing, render } from 'lit-html';
 import { styleMap } from 'lit-html/directives/style-map.js';
 
-const renderZones = (zones) =>
-  zones.map(({ color, name, link, start_date, end_date, travel_advice }) =>
-    html`
+const iconClickHandler = {
+  handleEvent(e) {
+    const warningId = e.target.getAttribute('data-warningid');
+    const warning = document.getElementById(warningId);
+
+    if (warning) {
+      warning.classList.remove('d-none');
+    }
+  }
+};
+
+const warningTemplate = ({ id, warning, color, font_color }) =>
+  warning && warning.reason && warning.reason.length
+    ?
+      html`
+        <div id="warning${id}"
+          class="d-none zone__warning"
+          style=${styleMap({ backgroundColor: color, color: font_color })}>
+          ${warning.reason}
+        </div>
+      `
+    : nothing;
+
+const renderZones = features =>
+  features.map(feature => {
+    const { color, font_color, name, link, start_date, end_date, travel_advice, warning }
+      = feature.properties;
+    return html`
       <article>
         <header>
-          <span style=${styleMap({ color: color })} class="material-icons">warning</span>
+          <span @click=${iconClickHandler}
+            data-warningid="warning${feature.id}"
+            style=${styleMap({ color: color })}
+            class="material-icons md-36">warning</span>
           <a class="zone__link" href="${link}">${name}</a>
         </header>
         <div class="zone__period">
@@ -21,10 +49,11 @@ const renderZones = (zones) =>
         </div>
         <p>${travel_advice}</p>
       </article>
-    `);
+      ${warningTemplate({ id: feature.id, warning, color, font_color })}
+    `});
 
 export default (features, targetElement) => {
   if (Array.isArray(features)) {
-    render(renderZones(features.map(f => f.properties)), targetElement);
+    render(renderZones(features), targetElement);
   }
 };
