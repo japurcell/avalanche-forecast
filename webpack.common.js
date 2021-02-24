b0c7a1464256605f02e5b1d7afa1b3408dabcc30
japurcell <japurcell@users.noreply.github.com>
2021-02-24 08:39:32 -0800

    Fixing webpack config merge issue

--- webpack.common.js
+++ webpack.common.js
@@ -3,65 +3,75 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const workbox = require('workbox-webpack-plugin');
 
-module.exports = {
-  entry: './js/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|webmanifest)$/i,
-        type: 'asset/resource',
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-        options: {
-          sources: false
-        }
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false
-    }),
-    new HtmlWebpackPlugin({
-      favicon: './favicon.ico',
-      template: './index.html'
-    }),
-    new workbox.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: [
+module.exports = (env, argv) => {
+  const configPath =
+    path.join(
+      __dirname,
+      'config',
+      (process.env.NODE_ENV || 'development') + '.js');
+
+  console.log('Config: ', configPath);
+
+  return {
+    entry: './js/app.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
         {
-          urlPattern: 'https://api.avalanche.org/v2/public/products/map-layer/NWAC',
-          handler: 'NetworkFirst'
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|webmanifest)$/i,
+          type: 'asset/resource',
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+          options: {
+            sources: false
+          }
         }
       ]
-    })
-  ],
-  resolve: {
-    alias: {
-      config: path.join(__dirname, 'config', (process.env.NODE_ENV || 'development') + '.js')
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false
+      }),
+      new HtmlWebpackPlugin({
+        favicon: './favicon.ico',
+        template: './index.html'
+      }),
+      new workbox.GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        runtimeCaching: [
+          {
+            urlPattern: 'https://api.avalanche.org/v2/public/products/map-layer/NWAC',
+            handler: 'NetworkFirst'
+          }
+        ]
+      })
+    ],
+    resolve: {
+      alias: {
+        config: configPath
+      }
     }
-  }
+  };
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
-module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
-});
+module.exports = (env, argv) => {
+  return merge(common(env, argv), {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    }
+  });
+};
